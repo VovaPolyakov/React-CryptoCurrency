625accb3a2c145b65697cdef844ccfbd7091f812
Vladimir <1337vovalenovo@gmail.com>
2023-02-28 00:29:13 +0300

    added dynamic trend information

--- src/components/CryptoItem.js
+++ src/components/CryptoItem.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.scss'
 
-const CryptoItem = ({item}) => {
+const CryptoItem = ({item,time}) => {
+    const [checked,setChecked] = useState(true)
+    const [trend,setTrend] = useState(true)
+    const handleChange = (e) => {
+        setChecked(!checked)
+    }
+    useEffect(() => {
+        if(Math.sign(item.quote.USD.percent_change_1h) === -1){
+            setTrend(false)
+        }
+    },[])
   return (
-    <p className={styles.crypto_name}>
-        {item.name}
-    </p>
+    <div className={styles.crypto_item}>
+        <p className={styles.crypto_name}>
+            {item.name}
+        </p>
+        <div className={styles.crypto_info}>
+            {item && <p>{`${item.quote.USD.percent_change_1h.toFixed(2)}%`}</p>}
+            
+            {trend ? <i className={styles.gg_trending}></i> : <i className={styles.gg_trending_down}></i>}
+            <div className={styles.crypto_price}>{`${item.quote.USD.price.toFixed(2)}$`}</div>
+            <label className={styles.crypto_checkbox}>
+                <input checked={checked} onChange={handleChange} type="checkbox"></input>
+                <div className={styles.checkmark}>
+                    <svg viewBox="0 0 256 256">
+                        <rect fill="none" height="256" width="256"></rect>
+                        <path d="M224.6,51.9a59.5,59.5,0,0,0-43-19.9,60.5,60.5,0,0,0-44,17.6L128,59.1l-7.5-7.4C97.2,28.3,59.2,26.3,35.9,47.4a59.9,59.9,0,0,0-2.3,87l83.1,83.1a15.9,15.9,0,0,0,22.6,0l81-81C243.7,113.2,245.6,75.2,224.6,51.9Z" strokeWidth="20px" stroke="#FFF" fill="none"></path>
+                    </svg>
+                </div>
+            </label>
+        </div>
+    </div>
   )
 }
 
--- src/components/CtyptoList.js
+++ src/components/CtyptoList.js
@@ -3,15 +3,47 @@ import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import CryptoItem from './CryptoItem'
 import styles from '../styles/Home.module.scss'
+import {IoIosArrowDropdown} from 'react-icons/io'
 
 const CryptoList = () => {
     const [data,setData] = useState()
     const items = useSelector((state) => state.crypto.data.data)
+    const [time,setTime] = useState('1h')
+    const [menu,setMenu] = useState(false)
+    const handleClick = (e) => {
+        setMenu(!menu)
+    }
+    const changeTime = (e) => {
+        console.log(e.target.id)
+        setTime(e.target.id)
+    }
   return (
-    <div className={styles.crypto_list}>
-      {items?.slice(0,10).map((item) => (
-        <CryptoItem key={item.id} item={item}/>
-      ))}
+    <div className={styles.crypto}>
+        <div className={styles.crypto_title}>
+            <h1>Favorite Crypto</h1>
+            <div className={styles.crypto_filter} onClick={handleClick}>
+                <button className={styles.crypto_time}>
+                    Filter: {time}
+                </button>
+                <IoIosArrowDropdown/>
+            </div>
+            {menu ? 
+                <nav className={styles.dropdown_menu}>
+                    <ul>
+                        <li id='1h' onClick={changeTime}>1h</li>
+                        <li id='24h' onClick={changeTime}>24h</li>
+                        <li id='7d' onClick={changeTime}>7d</li>
+                    </ul>
+                </nav>
+             : ''
+
+            }
+        </div>
+        <div className={styles.crypto_list}>
+            {items?.slice(0,10).map((item) => (
+                <CryptoItem time={time} key={item.id} item={item}/>
+            ))}
+        </div>
     </div>
   )
 }
