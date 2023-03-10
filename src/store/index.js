0c2f9f3304026c2a3ea0b594563a6af9c6b9d7db
Vladimir <1337vovalenovo@gmail.com>
2023-03-10 23:04:13 +0300

    delete react-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import { createStore,applyMiddleware,compose } from 'redux';
 import { initialState } from './initialState';
 import { rootReducer } from './rootReducer';
-import thunkMiddleware from 'redux-thunk'
+
 
 const data = createStore(
   rootReducer,
   initialState,
-  compose(applyMiddleware(thunkMiddleware),  window.__REDUX_DEVTOOLS_EXTENSION__())
+  window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default data;
